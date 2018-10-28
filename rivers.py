60a2e36ae1442072a5b843be11b19d14b956b1c2
Nick Sylva <nick.sylva@protonmail.com>
2018-10-28 10:15:54 -0700

    Initial templating

---
+++ rivers.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import json
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+	return render_template("project.html")
+
+
+if __name__ == '__main__':
+	app.run(port=5000,debug = True)
+
